c2fda71c8854e33bcf38538d85d979da1a07616f
acongos92 <30706447+acongos92@users.noreply.github.com>
2018-10-23 16:43:05 -0400

    Include unique domains

--- popup.js
+++ popup.js
@@ -5,6 +5,9 @@
 function getCookieCountAsync(){
     chrome.cookies.getAll({}, function(store){
         writeCookieCount(store.length)
+        let uniqueDomains = countUniqueDomains(store);
+        writeUniqueDomainCount(uniqueDomains);
+        console.log(store);
     });
 }
 
@@ -15,9 +18,33 @@ function getCookieCount(){
     getCookieCountAsync();
 }
 
+/*
+ * writes cookie count to popup page
+ */ 
 function writeCookieCount(count) {
-    var display = document.getElementById("cookieCountId");
-    display.innerHTML = count;
+    var display = document.getElementById("cookieCount");
+    display.innerHTML = "cookie count: " + count;
+}
+
+/*
+ * writes unique domains to popup page 
+ */
+function writeUniqueDomainCount(count){
+    let uniqueDomains = document.getElementById("uniqueCookieDomains");
+    uniqueDomains.innerHTML = "unique domains: " + count;
+}
+
+/*
+ * counts the unique cookie domains 
+ */
+function countUniqueDomains(cookies){
+    let uniques = [];
+    for (let i = 0; i < cookies.length; i++){
+        if(!uniques.includes(cookies[i].domain)){
+            uniques.push(cookies[i].domain)
+        }
+    }
+    return uniques.length;
 }
 
 
