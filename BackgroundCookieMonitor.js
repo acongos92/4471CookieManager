ea7d34393e4416931a5e798d8b1fbab336cc00b6
acongos92 <30706447+acongos92@users.noreply.github.com>
2018-10-25 21:02:41 -0400

    add object definitions and shell

--- BackgroundCookieMonitor.js
+++ BackgroundCookieMonitor.js
@@ -1,7 +1,41 @@
+/**
+ * some object definitions 
+ * RecentCookieData{
+ *                  name: "string cookie name"
+ *                  domain: "string cookie domain"
+ *                  storeTime: "datetime this object was added to storage"
+ *                  }
+ * RecentCookies {
+ *                  data: [] //an array of RecentCookieData
+ *               }
+ * 
+ * BlockedDomainData{
+ *                  domain: "string blocked domain"
+ *
+ *                  }
+ * 
+ * BlockedDomains {
+ *                  domains[] //an array of BlockedDomainData
+ *                }
+ */
+function addCookieToRecentStorage(cookie){
+
+}
+
+function checkStorageResult(result, cookie){
+    //this storage entry existed
+    if (result.RecentCookies){
+        
+    }
+}
+
+function getRecentCookiesAndAddIfNeeded(cookie){
+    chrome.storage.local.get("RecentCookies", function(result){
+        checkStorageResult(result, cookie)
+    });
+}
 chrome.cookies.onChanged.addListener(function(changeInfo){
     if(changeInfo.cause == "explicit"){
-        chrome.runtime.sendMessage({message: "cookieAdded", cookie: changeInfo}, function(response){
-            //nothing here 
-        })
+        getRecentCookiesAndAddIfNeeded(changeInfo.cookie)
     }
 });
